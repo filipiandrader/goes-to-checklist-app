dcd4a3274e0be06fb891c433fa1d863bf097d912
Filipi Andrade Rocha <filipiandrader@gmail.com>
2023-02-10 18:38:20 -0300

    Add new font and custom typography

--- app/src/main/java/com/far/goestochecklist/ui/theme/Type.kt
+++ app/src/main/java/com/far/goestochecklist/ui/theme/Type.kt
@@ -2,27 +2,39 @@ package com.far.goestochecklist.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.far.goestochecklist.R
 
-// Set of Material typography styles to start with
+val fonts = FontFamily(
+	Font(R.font.nexa_black, weight = FontWeight.Black),
+	Font(R.font.nexa_extra_bold, weight = FontWeight.ExtraBold),
+	Font(R.font.nexa_light, weight = FontWeight.Light),
+	Font(R.font.nexa_regular, weight = FontWeight.Normal)
+)
+
 val Typography = Typography(
+	h1 = TextStyle(
+		fontFamily = fonts,
+		fontWeight = FontWeight.Bold
+	),
+	h2 = TextStyle(
+		fontFamily = fonts,
+		fontWeight = FontWeight.Bold
+	),
 	body1 = TextStyle(
-		fontFamily = FontFamily.Default,
+		fontFamily = fonts,
 		fontWeight = FontWeight.Normal,
 		fontSize = 16.sp
+	),
+	body2 = TextStyle(
+		fontFamily = fonts,
+		fontWeight = FontWeight.Light
+	),
+	button = TextStyle(
+		fontFamily = fonts,
+		fontWeight = FontWeight.Black
 	)
-	/* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
