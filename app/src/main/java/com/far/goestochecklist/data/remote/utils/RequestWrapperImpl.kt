d7fec3cbb6e1e1666bf9ad26bfe0393ee08bec97
Filipi Andrade Rocha <filipiandrader@gmail.com>
2023-04-01 10:58:47 -0300

    Adjust splash bg; Throw exception with generic message error

--- app/src/main/java/com/far/goestochecklist/data/remote/utils/RequestWrapperImpl.kt
+++ app/src/main/java/com/far/goestochecklist/data/remote/utils/RequestWrapperImpl.kt
@@ -1,10 +1,12 @@
 package com.far.goestochecklist.data.remote.utils
 
 import com.far.goestochecklist.common.Constants
+import com.far.goestochecklist.common.Constants.GENERIC_ERROR
 import com.far.goestochecklist.common.fromJson
 import com.far.goestochecklist.data.remote.dto.GenericResponse
 import com.far.goestochecklist.domain.exception.DataSourceException
 import retrofit2.HttpException
+import java.net.SocketTimeoutException
 
 class RequestWrapperImpl : RequestWrapper {
 
@@ -14,7 +16,7 @@ class RequestWrapperImpl : RequestWrapper {
 		} catch (httpException: HttpException) {
 			throw handleException(httpException)
 		} catch (exception: Exception) {
-			throw exception
+			throw Exception(GENERIC_ERROR)
 		}
 	}
 
@@ -25,8 +27,8 @@ class RequestWrapperImpl : RequestWrapper {
 		val message = messageResponse?.message
 		val code = messageResponse?.code
 		return DataSourceException(
-			message = message ?: Constants.GENERIC_ERROR,
-			code = code ?: "",
+			message = message ?: GENERIC_ERROR,
+			code = code.orEmpty(),
 			cause = httpException
 		)
 	}
