7e5fef22f11d3de22e715b070e10c362c7aa634b
Filipi Andrade Rocha <filipiandrader@gmail.com>
2023-04-13 20:44:38 -0300

    Created search text field component; Creating bottom sheet to show filter options

--- app/src/main/java/com/far/goestochecklist/ui/components/textfield/SearchTextField.kt
+++ app/src/main/java/com/far/goestochecklist/ui/components/textfield/SearchTextField.kt
@@ -1,7 +1,103 @@
 package com.far.goestochecklist.ui.components.textfield
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.far.goestochecklist.R
+import com.far.goestochecklist.ui.theme.Gray800
+
 /*
  * Created by Filipi Andrade Rocha on 13/04/2023.
  */
- 
- 
+
+@Composable
+fun GoesToChecklistSearchTextField(
+	modifier: Modifier,
+	textValue: String,
+	onValueChange: (String) -> Unit,
+	onFilterClickListener: () -> Unit
+) {
+	Box(
+		modifier = Modifier
+			.fillMaxWidth()
+			.height(50.dp)
+			.background(Gray800, RoundedCornerShape(50.dp))
+	) {
+		Row(
+			modifier = modifier,
+			verticalAlignment = Alignment.CenterVertically
+		) {
+			Spacer(modifier = Modifier.size(2.dp))
+			Box(
+				modifier = Modifier
+					.size(45.dp)
+					.weight(0.35f)
+					.border(
+						width = 2.dp,
+						color = Color.White,
+						shape = RoundedCornerShape(40.dp)
+					),
+				contentAlignment = Alignment.Center
+			) {
+				Icon(
+					modifier = Modifier.size(35.dp),
+					painter = painterResource(id = R.drawable.ic_search),
+					contentDescription = stringResource(id = R.string.content_description_search_icon),
+					tint = Color.White
+				)
+			}
+			Spacer(modifier = Modifier.size(4.dp))
+			Column(
+				modifier = Modifier
+					.fillMaxWidth()
+					.weight(2f)
+			) {
+				Spacer(modifier = Modifier.size(2.dp))
+				TextField(
+					modifier = Modifier.fillMaxSize(),
+					value = textValue,
+					onValueChange = onValueChange,
+					placeholder = {
+						Text(
+							text = stringResource(id = R.string.search_text_field_placeholder),
+							style = MaterialTheme.typography.body2
+						)
+					},
+					colors = TextFieldDefaults.textFieldColors(
+						backgroundColor = Color.Transparent,
+						cursorColor = Color.White,
+						focusedIndicatorColor = Color.Transparent,
+						unfocusedIndicatorColor = Color.Transparent,
+					)
+				)
+			}
+			Spacer(modifier = Modifier.size(4.dp))
+			Box(
+				modifier = Modifier
+					.size(45.dp)
+					.weight(0.35f),
+				contentAlignment = Alignment.Center
+			) {
+				Icon(
+					modifier = Modifier
+						.size(32.dp)
+						.clickable { onFilterClickListener.invoke() },
+					painter = painterResource(id = R.drawable.ic_filter),
+					contentDescription = stringResource(id = R.string.content_description_filter_icon),
+					tint = Color.White
+				)
+			}
+			Spacer(modifier = Modifier.size(4.dp))
+		}
+	}
+}
--- app/src/main/java/com/far/goestochecklist/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/far/goestochecklist/ui/screens/search/SearchScreen.kt
@@ -1,119 +1,222 @@
 package com.far.goestochecklist.ui.screens.search
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.far.goestochecklist.R
-import com.far.goestochecklist.ui.theme.Gray800
+import com.far.goestochecklist.ui.components.button.GoesToChecklistButton
+import com.far.goestochecklist.ui.components.button.GoesToChecklistOutlinedButton
+import com.far.goestochecklist.ui.components.textfield.GoesToChecklistSearchTextField
+import com.far.goestochecklist.ui.theme.Gray700
 import com.far.goestochecklist.ui.theme.Gray900
 import com.far.goestochecklist.ui.theme.Yellow
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 /*
  * Created by Filipi Andrade Rocha on 10/04/2023.
  */
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SearchScreen(
 	bottomNavController: NavController
 ) {
 
-	Box(
-		modifier = Modifier
-			.fillMaxSize()
-			.background(color = Gray900)
+	var textValue by remember { mutableStateOf("") }
+	val bottomSheetState = rememberModalBottomSheetState(
+		initialValue = ModalBottomSheetValue.Hidden,
+		confirmValueChange = { it != ModalBottomSheetValue.HalfExpanded }
+	)
+	val coroutineScope = rememberCoroutineScope()
+
+	ModalBottomSheetLayout(
+		modifier = Modifier.fillMaxSize(),
+		sheetState = bottomSheetState,
+		sheetShape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp),
+		sheetContent = { FilterBottomSheet() }
 	) {
-		Column(modifier = Modifier.fillMaxSize()) {
-			Column(
-				modifier = Modifier
-					.fillMaxWidth()
-					.height(130.dp)
-					.background(Yellow)
-					.padding(16.dp)
-			) {
-				Spacer(modifier = Modifier.size(32.dp))
-				Box(
+		Box(
+			modifier = Modifier
+				.fillMaxSize()
+				.background(color = Gray900)
+		) {
+			Column(modifier = Modifier.fillMaxSize()) {
+				Column(
 					modifier = Modifier
 						.fillMaxWidth()
-						.height(50.dp)
-						.background(Gray800, RoundedCornerShape(50.dp))
+						.height(130.dp)
+						.background(Yellow)
+						.padding(16.dp)
 				) {
-					Row(
+					Spacer(modifier = Modifier.size(40.dp))
+					GoesToChecklistSearchTextField(
 						modifier = Modifier.fillMaxSize(),
-						verticalAlignment = Alignment.CenterVertically
+						textValue = textValue,
+						onValueChange = { textValue = it }
 					) {
-						Spacer(modifier = Modifier.size(2.dp))
-						Box(
-							modifier = Modifier
-								.size(45.dp)
-								.weight(0.35f)
-								.border(
-									width = 2.dp,
-									color = Color.White,
-									shape = RoundedCornerShape(40.dp)
-								),
-							contentAlignment = Alignment.Center
-						) {
-							Icon(
-								modifier = Modifier.size(35.dp),
-								painter = painterResource(id = R.drawable.ic_search),
-								contentDescription = "",
-								tint = Color.White
-							)
+						coroutineScope.launch {
+							when (bottomSheetState.isVisible) {
+								true -> bottomSheetState.hide()
+								false -> bottomSheetState.show()
+							}
 						}
-						Spacer(modifier = Modifier.size(4.dp))
-						Column(
-							modifier = Modifier
-								.fillMaxWidth()
-								.weight(2f)
-						) {
-							Spacer(modifier = Modifier.size(2.dp))
-							var text by remember { mutableStateOf("") }
-							TextField(
-								modifier = Modifier.fillMaxSize(),
-								value = text,
-								onValueChange = { text = it },
-								placeholder = {
-									Text(
-										text = "Pesquisar filme...",
-										style = MaterialTheme.typography.body2
-									)
-								},
-								colors = TextFieldDefaults.textFieldColors(
-									backgroundColor = Color.Transparent,
-									cursorColor = Color.White,
-									focusedIndicatorColor = Color.Transparent,
-									unfocusedIndicatorColor = Color.Transparent,
-								)
-							)
-						}
-						Spacer(modifier = Modifier.size(4.dp))
-						Box(
-							modifier = Modifier
-								.size(45.dp)
-								.weight(0.35f),
-							contentAlignment = Alignment.Center
-						) {
-							Icon(
-								modifier = Modifier.size(32.dp),
-								painter = painterResource(id = R.drawable.ic_filter),
-								contentDescription = "",
-								tint = Color.White
-							)
-						}
-						Spacer(modifier = Modifier.size(4.dp))
 					}
 				}
 			}
 		}
 	}
 
+	BackHandler(bottomSheetState.isVisible) {
+		coroutineScope.launch { bottomSheetState.hide() }
+	}
+}
+
+@Composable
+fun FilterBottomSheet() {
+	val year = listOf("2023", "2022", "2021", "2020", "2019", "2018")
+	val categories = listOf(
+		"Melhor Filme",
+		"Melhor Atriz",
+		"Melhor Ator",
+		"Melhor Direção",
+		"Melhor Atriz Coadjuvante",
+		"Melhor Ator Coadjuvante",
+		"Melhor Roteiro Original",
+		"Melhor Roteiro Adaptado",
+		"Melhor Fotografia",
+		"Melhor Figurino",
+		"Melhor Edição",
+		"Melhor Cabelo e Maquiagem",
+		"Melhor Design de Produção",
+		"Melhor Som",
+		"Melhor Canção Original",
+		"Melhor Efeitos Visuais",
+		"Melhor Trilha Sonora Original",
+		"Melhor Animação",
+		"Melhor Curta de Animação",
+		"Melhor Curta Live Action",
+		"Melhor Documentário",
+		"Melhor Documentário Curta-Metragem",
+		"Melhor Filme Internacional"
+	)
+
+	Column(
+		modifier = Modifier
+			.fillMaxSize()
+			.background(color = Gray700)
+	) {
+		Column(
+			modifier = Modifier
+				.fillMaxSize()
+				.padding(16.dp)
+		) {
+			Divider(
+				modifier = Modifier
+					.width(100.dp)
+					.height(5.dp)
+					.align(CenterHorizontally)
+					.background(color = Gray900, shape = CircleShape)
+			)
+			Spacer(modifier = Modifier.size(16.dp))
+			Text(
+				text = "Filtrar",
+				style = MaterialTheme.typography.body2,
+				fontWeight = FontWeight.Bold
+			)
+			Spacer(modifier = Modifier.size(8.dp))
+			Text(
+				text = "Ano",
+				style = MaterialTheme.typography.body2
+			)
+			Spacer(modifier = Modifier.size(4.dp))
+			LazyVerticalGrid(
+				modifier = Modifier.wrapContentSize(),
+				columns = GridCells.Fixed(5)
+			) {
+				items(year) {
+					Box(
+						modifier = Modifier
+							.width(60.dp)
+							.height(45.dp)
+							.padding(end = 8.dp, bottom = 8.dp)
+							.border(
+								width = 1.dp,
+								color = Color.White,
+								shape = RoundedCornerShape(4.dp)
+							),
+						contentAlignment = Center
+					) {
+						Text(text = it, style = MaterialTheme.typography.body2)
+					}
+				}
+			}
+			Spacer(modifier = Modifier.size(8.dp))
+			Text(
+				text = "Categoria",
+				style = MaterialTheme.typography.body2
+			)
+			Spacer(modifier = Modifier.size(4.dp))
+			LazyVerticalGrid(
+				columns = GridCells.Fixed(4)
+			) {
+				items(categories) {
+					Box(
+						modifier = Modifier
+							.wrapContentSize()
+							.padding(end = 8.dp, bottom = 8.dp)
+							.border(
+								width = 1.dp,
+								color = Color.White,
+								shape = RoundedCornerShape(4.dp)
+							)
+					) {
+						Box(
+							modifier = Modifier
+								.wrapContentSize()
+								.padding(8.dp),
+							contentAlignment = Center
+						) {
+							Text(text = it, style = MaterialTheme.typography.body2)
+						}
+					}
+				}
+			}
+			Spacer(modifier = Modifier.size(8.dp))
+			Row {
+				GoesToChecklistButton(
+					modifier = Modifier
+						.wrapContentWidth()
+						.height(42.dp)
+						.weight(1f),
+					buttonText = "Filtrar filmes",
+					isEnable = true,
+					onClick = { /*TODO*/ })
+				Spacer(modifier = Modifier.size(8.dp))
+				GoesToChecklistOutlinedButton(
+					modifier = Modifier
+						.wrapContentWidth()
+						.height(42.dp)
+						.weight(1f),
+					buttonText = "Limpar filtros",
+					isEnable = true,
+					onClick = { /*TODO*/ })
+			}
+		}
+	}
 }
